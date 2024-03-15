bd0ce535f3107b595960b504ae96e05dff585591
emiferreyra89 <ferreyra89@gmail.com>
2024-03-15 00:49:48 -0300

    Agregue todos los endpoints correspondientes al modelo Artista

--- src/controllers/api/artistasAPIController.js
+++ src/controllers/api/artistasAPIController.js
@@ -1,18 +1,79 @@
 const path = require('path');
-const db = require('../../database/models');
+const db = require('../../database/models/index');
+const { log } = require('console');
 
 const artistasAPIController = {
-    'list': (req, res) => {
-        return res.send('Devuelve datos de los artistas.');
+    list: async (req, res) => {
+        try {
+            await db.Artista.findAll()
+            .then(artistas => {
+                return res.status(200).json({
+                    data: artistas,
+                    total: artistas.length,
+                    status: 200,
+                    url:'/api/artistas'
+                })
+            })
+            //return res.send(artistas);
+        } catch (error) {
+            res.status(400).send(error.message)
+        }
     },
-    create: (req,res) => {
-        return res.send('Puedes agregar un nuevo artista a nuestra base de datos. Para porder utilizar esta API, debes ejecutar la ruta /artistas/create/id desde la plataforma POSTMAN');
+
+    create: async (req,res) => {
+        try {
+            const {nombre} = req.body
+            await db.Artista.create({nombre})
+            .then(artista => {
+                return res.status(200).json({
+                    data: artista,
+                    status: 200,
+                    url:'/api/artistas/create'
+                })
+            })
+            //return res.send('Puedes agregar un nuevo artista a nuestra base de datos. Para porder utilizar esta API, debes ejecutar la ruta /artistas/create/id desde la plataforma POSTMAN');
+        } catch (error) {
+            console.log(error.message);
+        }
     },
-    update: (req,res) => {
-        return res.send('Puedes modifcar el nombre del artista en nuestra base de datos. Para porder utilizar esta API, debes ejecutar la ruta /artistas/update/id desde la plataforma POSTMAN');
+
+    update: async (req,res) => {
+        try {
+            const {id} = req.params
+            const {nombre} = req.body
+            await db.Artista.update({nombre},{where:{id}})
+            .then(() => {
+                return res.status(200).json({
+                    id: id,
+                    data: 'update Ok',
+                    status: 200,
+                    url:'/api/artistas/update/:id'
+                })
+            }) 
+            //return res.send('Puedes modifcar el nombre del artista en nuestra base de datos. Para porder utilizar esta API, debes ejecutar la ruta /artistas/update/id desde la plataforma POSTMAN');
+        } catch (error) {
+            console.log(error.message);
+        }
     },
-    destroy: (req,res) => {
-        return res.send('Puedes eliminar un artista a nuestra base de datos. Para porder utilizar esta API, debes ejecutar la ruta /artistas/delete/id desde la plataforma POSTMAN');
+
+    destroy: async (req,res) => {
+        try {
+            const {id} = req.params
+            await db.Artista.destroy({
+            where: {id}, force: true
+            })
+            .then(() => {
+                return res.status(200).json({
+                    id: id,
+                    data: 'delete Ok',
+                    status: 200,
+                    url:'/api/artistas/delete/:id'
+                })
+            })
+            //return res.send('Puedes eliminar un artista a nuestra base de datos. Para porder utilizar esta API, debes ejecutar la ruta /artistas/delete/id desde la plataforma POSTMAN');
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 }
 module.exports = artistasAPIController;
