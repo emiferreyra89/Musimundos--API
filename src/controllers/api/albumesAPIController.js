bda041693206903d321606584cee96af6585561b
emiferreyra89 <ferreyra89@gmail.com>
2024-03-15 02:56:28 -0300

    Agregue los endpoints relacionados al modelo Album

--- src/controllers/api/albumesAPIController.js
+++ src/controllers/api/albumesAPIController.js
@@ -1,13 +1,67 @@
 const path = require('path');
 const db = require('../../database/models');
+const { query } = require('express');
 
 const albumesAPIController = {
-    'list': (req, res) => {
-        return res.send('Devuelve la información de todos los albumes asociados con cada uno de los artistas registrados en nuestra plataforma.')
+    list: async (req, res) => {
+        try {
+            await db.Album.findAll({
+                include: [{association: 'artista'}]
+            })
+            .then(albumes => {
+                return res.status(200).json({
+                    data: albumes,
+                    total: albumes.length,
+                    status: 200,
+                    url:'/api/albumes'
+                })
+            })
+            //return res.send('Devuelve la información de todos los albumes asociados con cada uno de los artistas registrados en nuestra plataforma.')
+        } catch (error) {
+            console.log(error.message);
+        }
     },
 
-    'detail': (req, res) => {
-        return res.send('Devuelve el detalle de un album asociado a un (" id ") de un artista indicado en la ruta o en la query string.');
+    ///...Detalle de un álbum asociado a un ('id') de un artista indicado en la ruta...
+    // detail: async (req, res) => {
+    //     try {
+    //         const {id} = req.params
+    //         await db.Album.findOne({
+    //             where: {id_artista: id},
+    //             include:[{association:'artista'}]
+    //         })
+    //         .then(album => {
+    //             return res.status(200).json({
+    //                 data: album,
+    //                 status: 200,
+    //                 url:'/api/album/:id'
+    //             })
+    //         })
+    //         // return res.send('Devuelve el detalle de un album asociado a un (" id ") de un artista indicado en la ruta o en la query string.');
+    //     } catch (error) {
+    //         res.send(error.message);
+    //     }
+    // },
+
+    ///...Detalle de un álbum segun ('id') indicado en la ruta, asociado a un de un artista...
+    detail: async (req, res) => {
+        try {
+            const {id} = req.params
+            await db.Album.findOne({
+                where: {id},
+                include:[{association:'artista'}]
+            })
+            .then(album => {
+                return res.status(200).json({
+                    data: album,
+                    status: 200,
+                    url:'/api/album/:id'
+                })
+            })
+            // return res.send('Devuelve el detalle de un album asociado a un (" id ") de un artista indicado en la ruta o en la query string.');
+        } catch (error) {
+            res.send(error.message);
+        }
     }
 }
 
